1262741932b7729cbd72f850583161a59375e15e
Anonforjob <71564759+Anonforjob@users.noreply.github.com>
2020-10-01 18:38:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    document.getElementById('myImage').src='bulb/pic_bulbon.gif';
+}
+
+function myFunction2(){
+    document.getElementById('myImage').src='bulb/pic_bulboff.gif';
+}
